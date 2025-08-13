e81f99c8a73884037570dbc3df185494f02d0778
Harison Cleyton <harisonc081@gmail.com>
2025-08-13 09:49:33 -0300

    feat: Add variable environment for Google redirect URL.

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,7 +11,7 @@ export const configurePassport = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
       },
       async function (accessToken, refreshToken, profile, cb) {
         if (!profile) {
