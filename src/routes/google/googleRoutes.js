feaf7a75540fde6332384dc241219adc627b8f1d
Harison Cleyton <harisonc081@gmail.com>
2025-08-22 09:29:10 -0300

    fix(logs): Google logs

--- src/routes/google/googleRoutes.js
+++ src/routes/google/googleRoutes.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { insertLog } from '../../services/logService.js';
 import User from '../../models/userModel.js';
-import { decode } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+const { decode } = jwt;
 
 // select_account faz sempre pedir pra logar independente do navegador tiver uma conta salva
 router.get(
@@ -20,19 +21,26 @@ router.get(
   '/google/callback',
   passport.authenticate('google', {
     session: false,
-    failureRedirect: '/login', // enviar pro front
+    failureRedirect: '/login',
   }),
   async (req, res) => {
-    const token = req.user;
-    const { email } = decode(token.token);
-    const user = await User.findOne({ where: { email: email } });
+    const tokenSend = req.user; 
+    const decoded = decode(tokenSend.token);
 
+    if (!decoded) {
+      return res.redirect('/login');
+    }
+
+    const email = decoded.email;
+
+    const user = await User.findOne({ where: { email } });
     await insertLog(user.user_id, 'login_google', req);
+
     res.redirect(
       process.env.URL_FRONT_LOGIN.replace(
         'action',
-        token.register === true ? 'register' : 'login'
-      ).replace('token', token.token)
+        tokenSend.register === true ? 'register' : 'login'
+      ).replace('token', tokenSend.token)
     );
   }
 );
